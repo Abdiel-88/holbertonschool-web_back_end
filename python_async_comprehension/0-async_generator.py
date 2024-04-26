4785ba67ac835907c1e6d24f39ab35faf55aa2bc
Abdiel-88 <abdiel.f@outlook.com>
2024-04-26 12:48:01 -0400

    task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,17 +1,18 @@
+#!/usr/bin/env python3
 import asyncio
 import random
 
 async def async_generator():
-    """
-    Asynchronous generator that yields random integers between 0 and 10.
-
-    This generator uses the `asyncio.sleep()` function to introduce a delay of 1 second
-    between each yield. It generates a total of 10 random integers.
-
-    Yields:
-        int: A random integer between 0 and 10.
-
-    """
+    """Generate random numbers asynchronously."""
     for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        await asyncio.sleep(1)  # Asynchronously wait 1 second
+        yield random.uniform(0, 10)  # Yield a random number between 0 and 10
+
+async def print_yielded_values():
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    print(result)
+
+if __name__ == "__main__":
+    asyncio.run(print_yielded_values())
