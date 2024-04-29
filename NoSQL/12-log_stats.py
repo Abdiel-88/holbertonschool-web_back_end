94ea293cf16a117432b0fb1c0a1afc7869395d21
Abdiel-88 <abdiel.f@outlook.com>
2024-04-29 14:32:35 -0400

    fix task 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,7 +3,6 @@
 
 from pymongo import MongoClient
 
-
 def log_stats():
     """ Function to print stats about Nginx logs. """
     client = MongoClient('mongodb://127.0.0.1:27017')
@@ -22,10 +21,8 @@ def log_stats():
         print(f"    method {method}: {count}")
 
     # Count of documents where method is GET and path is /status
-    status_checks = nginx_collection.count_documents
-    ({'method': 'GET', 'path': '/status'})
+    status_checks = nginx_collection.count_documents({'method': 'GET', 'path': '/status'})
     print(f"{status_checks} status check")
-
 
 if __name__ == "__main__":
     log_stats()
