fd953c4fb79c74e1ef26a667ffd9ae8609fd8209
Abdiel-88 <abdiel.f@outlook.com>
2024-04-30 13:14:22 -0400

    task 0 fix and task 1

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
-"""
-Module Description: Implements a function named
-`index_range` which calculates the start and end index
-for pagination, given the page number and the size of the page.
+''' Description: Write a function named index_range that takes two integer
+                 arguments page and page_size.
 
-This function is designed to handle pagination
-calculations where page numbers start from 1.
-"""
+'''
 
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    start = (page - 1) * page_size
-    end = page * page_size
-    return (start, end)
+    ''' Return tuple containing pagination start index and end index. '''
+    return ((page_size * (page - 1)), page_size * page)
