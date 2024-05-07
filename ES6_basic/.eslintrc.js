ff7327ed6842c7f307c6261d39d9dd3cab3b5da2
Abdiel-88 <abdiel.f@outlook.com>
2024-05-07 13:25:50 -0400

    setup and task 0

---
+++ ES6_basic/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    env: {
+      es6: true,
+      node: true,
+      jest: true, // if you are using Jest
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    rules: {
+      // add any specific rules you want
+    },
+  };
+  
