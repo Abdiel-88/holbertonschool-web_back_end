effa83888429e9909616db70419ea05091540983
Abdiel-88 <abdiel.f@outlook.com>
2024-05-08 16:57:00 -0400

    task 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,12 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  // Initialize task variables with default values
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true;// Affecting the outer scope directly
-    task2 = false;// Affecting the outer scope directly
+    // Update task variables based on the input parameter
+    const task = true;
+    const task2 = false;
   }
 
-  console.log(`Final values: task = ${task}, task2 = ${task2}`);
   return [task, task2];
 }
