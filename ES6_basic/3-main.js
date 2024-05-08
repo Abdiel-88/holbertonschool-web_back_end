be0137c3c6029d709acb24285f55db6ed5900bc2
Abdiel-88 <abdiel.f@outlook.com>
2024-05-07 22:14:47 -0400

    task 3, still getting no points

---
+++ ES6_basic/3-main.js
@@ -0,0 +1,6 @@
+// 3-main.js
+import getSumOfHoods from './3-default-parameter.js';
+
+console.log(getSumOfHoods(34)); // Output: 142
+console.log(getSumOfHoods(34, 3)); // Output: 56
+console.log(getSumOfHoods(34, 3, 4)); // Output: 41
