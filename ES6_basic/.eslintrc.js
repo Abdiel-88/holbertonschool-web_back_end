89a9709d9f0d466bd2505bb43b9cb87158f1a5ab
Abdiel-88 <abdiel.f@outlook.com>
2024-05-08 19:22:24 -0400

    i dont know anymore

--- ES6_basic/.eslintrc.js
+++ ES6_basic/.eslintrc.js
@@ -1,36 +1,32 @@
-
 module.exports = {
-    env: {
-      browser: false,
-      es6: true,
-      jest: true,
-    },
-    extends: [
-      'airbnb-base',
-      'plugin:jest/all',
-    ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
+    languageOptions: {
       ecmaVersion: 2018,
       sourceType: 'module',
+      globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+      }
     },
-    plugins: ['jest'],
+    plugins: {
+      jest: require('eslint-plugin-jest'),
+    },
     rules: {
       'no-console': 'off',
       'no-shadow': 'off',
       'no-restricted-syntax': [
         'error',
         'LabeledStatement',
-        'WithStatement',
+        'WithStatement'
       ],
+      'eqeqeq': ['error', 'always'],  // Enforce the use of === and !==
+      'no-var': 'error',              // Require let or const instead of var
+      'prefer-const': 'error',        // Suggest using const
+      'no-unused-vars': 'warn',       // Warn about variables that are declared but not used
+      'no-undef': 'error',            // Disallow undeclared variables
+      'consistent-return': 'error'    // Require return statements to either always or never specify values
     },
-    overrides:[
-      {
-        files: ['*.js'],
-        excludedFiles: 'babel.config.js',
-      }
-    ]
+    linterOptions: {
+      envs: ['node', 'jest']
+    }
   };
+  
