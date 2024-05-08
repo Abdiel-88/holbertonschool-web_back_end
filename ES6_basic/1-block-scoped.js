eb945771e12051e1ffda28ac909cf322747827bb
Abdiel-88 <abdiel.f@outlook.com>
2024-05-08 16:49:27 -0400

    task 1 fix lint

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-    let task = false;
-    let task2 = true;
-  
-    if (trueOrFalse) {
-      let task = true;    // This 'task' is scoped to the if block
-      let task2 = false;  // This 'task2' is scoped to the if block
-    }
-  
-    return [task, task2]; // Returns the original 'task' and 'task2' declared at the function level
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = true;// Affecting the outer scope directly
+    task2 = false;// Affecting the outer scope directly
   }
-  
+
+  console.log(`Final values: task = ${task}, task2 = ${task2}`);
+  return [task, task2];
+}
