39f70c497a5596bb30c84e7999eada2cc7a757b1
Abdiel-88 <abdiel.f@outlook.com>
2024-05-09 18:43:29 -0400

    task 7 again

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,7 +1,6 @@
 // 7-getBudgetObject.js
 
 export default function getBudgetObject(income, gdp, capita) {
-    // Ensure the object formatting exactly matches the expected output
     const budget = {
       income,
       gdp,
