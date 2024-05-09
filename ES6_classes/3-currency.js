f3901e8131bc961edaaa4bd7d3657f5b9ea3f744
Abdiel-88 <abdiel.f@outlook.com>
2024-05-08 22:44:44 -0400

    re do tasks

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,48 +1,26 @@
-class Currency {
+export default class Currency {
     constructor(code, name) {
-        // Ensure the code and name are strings
-        if (typeof code !== 'string') {
-            throw new TypeError('Code must be a string');
-        }
-        if (typeof name !== 'string') {
-            throw new TypeError('Name must be a string');
-        }
-
-        // Assign to the private variables
-        this._code = code;
-        this._name = name;
+      this._code = code;
+      this._name = name;
     }
-
-    // Getter for code
+  
+    set code(code) {
+      this._code = code;
+    }
+  
     get code() {
-        return this._code;
+      return this._code;
     }
-
-    // Setter for code
-    set code(newCode) {
-        if (typeof newCode !== 'string') {
-            throw new TypeError('Code must be a string');
-        }
-        this._code = newCode;
+  
+    set name(name) {
+      this._name = name;
     }
-
-    // Getter for name
+  
     get name() {
-        return this._name;
+      return this._name;
     }
-
-    // Setter for name
-    set name(newName) {
-        if (typeof newName !== 'string') {
-            throw new TypeError('Name must be a string');
-        }
-        this._name = newName;
-    }
-
-    // Method to display the currency in the desired format
+  
     displayFullCurrency() {
-        return `${this._name} (${this._code})`;
+      return `${this._name} (${this._code})`;
     }
-}
-
-export default Currency;
+  }
