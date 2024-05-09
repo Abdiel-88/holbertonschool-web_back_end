51683b71c3779eeb74b7ef4bd8088392a8e7db10
Abdiel-88 <abdiel.f@outlook.com>
2024-05-09 15:38:21 -0400

    start frsh

--- ES6_basic/1-main.js
+++
@@ -1,5 +0,0 @@
-// 1-main.js
-import taskBlock from './1-block-scoped.js';
-
-console.log(taskBlock(true));  // Expect to see [false, true]
-console.log(taskBlock(false)); // Expect to see [false, true]
