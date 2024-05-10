7e3a00e3acbaf11efdb7c4807063fd5ed4e7234d
Abdiel-88 <abdiel.f@outlook.com>
2024-05-10 12:39:59 -0400

    fix task 1 and 9

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,13 +1,6 @@
-export default function taskBlock(trueOrFalse) {
-  // Initialize task variables with default values
-  const task = false;
-  const task2 = true;
+export default function taskBlock() {
+  const resultTask = false;
+  const resultTask2 = true;
 
-  if (trueOrFalse) {
-    // Update task variables based on the input parameter
-    const task = true;
-    const task2 = false;
-  }
-
-  return [task, task2];
+  return [resultTask, resultTask2];
 }
